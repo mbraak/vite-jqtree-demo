d3bbee9286016b3bbc6e7699df43cffda1852a3a
Marco Braak <marco@floryn.com>
2023-08-26 11:52:40 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import "jqtree";
+import "jqtree/jqtree.css";
+
+var data = [
+  {
+    name: "node1",
+    id: 1,
+    children: [
+      { name: "child1", id: 2 },
+      { name: "child2", id: 3 },
+    ],
+  },
+  {
+    name: "node2",
+    id: 4,
+    children: [{ name: "child3", id: 5 }],
+  },
+];
+
+$("#app").tree({ data });
